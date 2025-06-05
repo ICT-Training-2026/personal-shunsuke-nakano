340470fc5134556b9d489ca8baebc491811f8856
github Shunsuke-Nakano36 <shunsuke-nakano@kccs.co.jp>
2025-06-05 13:20:52 +0900

    コンフリクト解消

--- ObjBasic0101/src/Product5.java
+++ ObjBasic0101/src/Product5.java
@@ -1,8 +1,5 @@
 
 public class Product5 {
-<<<<<<< HEAD
 	// ローカルリポジトリの修正
-=======
-  // コメント
->>>>>>> branch 'feature' of https://github.com/ICT-Training-2026/personal-shunsuke-nakano.git
+	// コメント
 }
